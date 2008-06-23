24ee5485d1bb0c86833977dbfbd2e4999dba8e16
Kelly Rauchenberger <fefferburbia@gmail.com>
2008-06-23 10:00:56 +0000

    Moved over to the Four Island Templating System.

--- central/trunk/includes/common.php
+++ central/trunk/includes/common.php
@@ -2,12 +2,13 @@
 
 /* InstaDisc Server - A Four Island Project */
 
-include('db.php');
+include('includes/db.php');
 
 $getconfig = "SELECT * FROM config";
 $getconfig2 = mysql_query($getconfig);
 $config = mysql_fetch_array($getconfig2);
 
-include('header.php');
+include('includes/template.php');
+include('includes/header.php');
 
 ?>
