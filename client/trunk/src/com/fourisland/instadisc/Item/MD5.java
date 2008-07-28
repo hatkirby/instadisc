d391fafac8927c7dac961250d2605e13690e8c17
hatkirby <hatkirby@localhost>
2008-07-28 15:00:27 +0000

    Added well-formed checks
    After recieving an Item, InstaDisc Client now checks the item for a status of being Well-Formed, as in, it has all of the required headers, the category exists, the category's required semantics are present and the verification is valid.

---
+++ client/trunk/src/com/fourisland/instadisc/Item/MD5.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fourisland.instadisc.Item;
+
+import java.security.MessageDigest;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author hatkirby
+ */
+public class MD5 {
+    
+    String ver;
+
+    public MD5(String ver) {
+        this.ver = ver;
+    }
+    
+    public String hash()
+    {
+        StringBuilder verify = new StringBuilder();
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            int i = 0;
+            byte[] create = new byte[ver.length()];
+            for (i = 0; i < ver.length(); i++) {
+                create[i] = (byte) ver.charAt(i);
+            }
+            byte buffer[] = md5.digest(create);
+            for (i = 0; i < buffer.length; i++) {
+                String hex = Integer.toHexString(buffer[i]);
+                verify.append(pad(hex.substring(max(hex.length() - 2, 0)),"0",2));
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(WellFormedItem.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return verify.toString();
+    }
+    
+    private int max(int x, int y)
+    {
+        return (x > y ? x : y);
+    }
+    
+    private String pad(String in, String pad, int len)
+    {
+        while (in.length() < len)
+        {
+            in = pad + in;
+        }
+        return in;
+    }
+}
