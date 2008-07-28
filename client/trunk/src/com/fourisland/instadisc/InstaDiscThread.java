8b426229b9f0d7b360e487871930b682f104dd9f
Kelly Rauchenberger <fefferburbia@gmail.com>
2008-07-28 10:00:25 +0000

    Added listening server
    The Client now listens for connections on port 4444, allowing multiple connections at once. Once a connection iis found, the Cliet waits until the connecton is broken, then downloads the sent dat, parses it into headers and passes it to the Item class.

---
+++ client/trunk/src/com/fourisland/instadisc/InstaDiscThread.java
@@ -0,0 +1,114 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fourisland.instadisc;
+
+import com.fourisland.instadisc.Item.Item;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author hatkirby
+ */
+public class InstaDiscThread implements Runnable {
+
+    boolean cancelled = false;
+
+    public void cancel() {
+        cancelled = true;
+    }
+
+    public void run() {
+        try {
+            ServerSocket svr = new ServerSocket();
+            java.net.InetSocketAddress addr = new java.net.InetSocketAddress(4444);
+            svr.bind(addr);
+            while (!cancelled) {
+                try {
+                    Socket s = svr.accept();
+                    HandleItemThread hit = new HandleItemThread(s);
+                    Thread hitt = new Thread(hit);
+                    hitt.start();
+                } catch (Exception ex) {
+                    cancel();
+                    Logger.getLogger(InstaDiscThread.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            svr.close();
+        } catch (IOException ex) {
+            Logger.getLogger(InstaDiscThread.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+}
+
+class HandleItemThread implements Runnable {
+
+    Socket s;
+
+    public HandleItemThread(Socket s) {
+        this.s = s;
+    }
+
+    public void run() {
+        try {
+            InputStream is = s.getInputStream();
+            int buffer[] = new int[1000];
+            int rs = 0;
+            int i = 0;
+
+            while (rs != -1) {
+                try {
+                    rs = is.read();
+
+                    if (rs != -1) {
+                        buffer[i] = rs;
+                    }
+                    i++;
+                } catch (IOException ex) {
+                    Logger.getLogger(HandleItemThread.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+            StringBuilder result = new StringBuilder();
+            int j = 0;
+            for (j = 0; j < i; j++) {
+                result.append(Character.toString((char) buffer[j]));
+            }
+
+            String[] headers = result.toString().split("\n");
+            HashMap<String, String> headerMap = new HashMap<String, String>();
+            i = 0;
+            while (1 == 1) {
+                try {
+                    String[] nameVal = headers[i].split(": ");
+                    String name = nameVal[0];
+                    String value = nameVal[1];
+                    headerMap.put(name, value);
+                } catch (Exception ex) {
+                    break;
+                }
+                i++;
+            }
+
+            Logger.getLogger(HandleItemThread.class.getName()).log(Level.INFO, headerMap.toString());
+            try {
+                s.close();
+            } catch (IOException ex) {
+                Logger.getLogger(HandleItemThread.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            Item idI = new Item(headerMap);
+            idI.start();
+        } catch (IOException ex) {
+            Logger.getLogger(HandleItemThread.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
