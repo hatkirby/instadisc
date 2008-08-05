71dc119b2769269578aa0e84e75cf5dbf3c625e8
Kelly Rauchenberger <fefferburbia@gmail.com>
2008-08-05 17:12:46 +0000

    Client: Modified IDSubscriptionListCellRenderer
    Changed it to show the category of a subscription in parentheses next to the title (for those cases where there are more than one subscriptions with the same title).

--- client/trunk/src/com/fourisland/instadisc/IDSubscriptionListCellRenderer.java
+++ client/trunk/src/com/fourisland/instadisc/IDSubscriptionListCellRenderer.java
@@ -17,7 +17,7 @@ import javax.swing.ListCellRenderer;
 class IDSubscriptionListCellRenderer extends JLabel implements ListCellRenderer {
 
     public Component getListCellRendererComponent(JList arg0, Object arg1, int arg2, boolean arg3, boolean arg4) {
-        this.setText(((Subscription) arg1).getTitle());
+        this.setText(((Subscription) arg1).getTitle() + " (" + ((Subscription) arg1).getCategory() + ")");
         
         if (arg3) {
             this.setForeground(arg0.getSelectionForeground());
