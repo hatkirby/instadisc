067029ed211db394f974fa1762fe217068533efb
hatkirby <hatkirby@localhost>
2008-08-24 19:55:44 +0000

    Update: Added password protection to phpBB3
    Closes #39

--- update/plugin/phpBB3/trunk/root/includes/functions_instadisc.php
+++ update/plugin/phpBB3/trunk/root/includes/functions_instadisc.php
@@ -24,7 +24,7 @@ include($phpbb_root_path . 'includes/xmlrpc/xmlrpc.inc');
 function sendItem($title, $userID, $url, $fourm)
 {
 	global $config, $db, $phpbb_root_path;
-	$verID = rand(1,2147483647);
+	$verID = rand(1,65536);
 
 	$da = array('user_id' => $userID);
 	$getuser = "SELECT * FROM " . USERS_TABLE . " WHERE " . $db->sql_build_array('SELECT', $da);
@@ -33,7 +33,7 @@ function sendItem($title, $userID, $url, $fourm)
 	$db->sql_freeresult($getuser2);
 	$author = $getuser3['username'];
 
-	$url = str_replace($phpbb_root_path, generate_board_url() . '/', $url);
+	$url = html_entity_decode(str_replace($phpbb_root_path, generate_board_url() . '/', $url));
 
 	$da = array('forum_id' => $fourm);
 	$getfourm = "SELECT * FROM " . FORUMS_TABLE . " WHERE " . $db->sql_build_array('SELECT', $da);
@@ -41,6 +41,29 @@ function sendItem($title, $userID, $url, $fourm)
 	$getfourm3 = $db->sql_fetchrow($getfourm2);
 	$db->sql_freeresult($getfourm2);
 	$semantics = array('forum' => $getfourm3['forum_name']);
+
+	$encID = 0;
+	if (($config['id_encryption_key'] != '') && extension_loaded('mcrypt'))
+	{
+		$encID = rand(1,2147483647);
+
+		$cipher = 'rijndael-128';
+		$mode = 'cbc';
+		$key = substr(md5(substr(str_pad($config['id_encryption_key'],16,$encID),0,16)),0,16);
+
+		$td = mcrypt_module_open($cipher, "", $mode, "");
+
+		$title = encryptString($td, $key, $title);
+		$author = encryptString($td, $key, $author);
+		$url = encryptString($td, $key, $url);
+
+                foreach ($semantics as $name => $value)
+                {
+                        $semantics[$name] = encryptString($td, $key, $value);
+                }
+
+                mcrypt_module_close($td);
+	}
 
 	$client = new xmlrpc_client($config['id_central_server']);
 	$msg = new xmlrpcmsg("InstaDisc.sendFromUpdate", array(	new xmlrpcval($config['id_username'], 'string'),
@@ -50,7 +73,8 @@ function sendItem($title, $userID, $url, $fourm)
 								new xmlrpcval($title, 'string'),
 								new xmlrpcval($author, 'string'),
 								new xmlrpcval($url, 'string'),
-								new xmlrpcval(serialize($semantics), 'string')));
+								new xmlrpcval(serialize($semantics), 'string'),
+								new xmlrpcval($encID, 'int')));
 	$resp = $client->send($msg);
 	$val = $resp->value()->scalarVal();
 
@@ -58,6 +82,15 @@ function sendItem($title, $userID, $url, $fourm)
 	{
 		sendItem($title, $userID, $url, $fourm);
 	}
+}
+
+function encryptString($td, $key, $string)
+{
+        mcrypt_generic_init($td, $key, strrev($key));
+        $string = bin2hex(mcrypt_generic($td, $string));
+        mcrypt_generic_deinit($td);
+
+        return $string;
 }
 
 ?>
