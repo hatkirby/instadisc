681a40af319373510f30793d3b892bd9700213fb
Kelly Rauchenberger <fefferburbia@gmail.com>
2008-08-25 14:17:08 +0000

    Central: Added Activate Subscription form
    Also added an option to the user panel that allows you to edit configuration values if you're an admin. Refs #25

---
+++ central/trunk/theme/activatedsub.tpl
@@ -0,0 +1,14 @@
+<HTML>
+	<HEAD>
+		<TITLE><!--SITENAME--> InstaDisc Central Server</TITLE>
+	</HEAD>
+
+	<BODY>
+		<CENTER>
+			<H1>InstaDisc Subscription Activation</H1>
+
+			<P>You've sucessfully activated your subscription! <A HREF="userpanel.php">Back to User Panel</A>
+		</CENTER>
+	</BODY>
+</HTML>
+
--- central/trunk/userpanel.php
+++ central/trunk/userpanel.php
@@ -10,6 +10,12 @@ if (isset($_SESSION['username']))
 	$template = new FITemplate('userpanel');
 	$template->add('SITENAME', instaDisc_getConfig('siteName'));
 	$template->add('USERNAME', $_SESSION['username']);
+
+	if (instaDisc_getConfig('owner') == $_SESSION['username'])
+	{
+		$template->adds_block('ADMIN',array('ex'=>1));
+	}
+
 	$template->display();
 } else {
 	header('Location: index.php');
