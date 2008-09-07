8ccc224160a7843d476b25ed866dd62426079e32
hatkirby <hatkirby@localhost>
2008-09-07 14:55:06 +0000

    Series: Started ACP
    Refs #53

---
+++ series/core/trunk/admin/main.php
@@ -0,0 +1,25 @@
+<?php
+
+/* InstaDisc Series - A Four Island Project */
+
+/** 
+ * require_once() is used to ensure
+ * the ACP files are being called by
+ * admin.php instead of their actual
+ * locations admin/.
+ * The _once() part ensures no problem
+ * arises as includes/instadisc.php has
+ * already been included from admin.php
+ */
+require_once('includes/instadisc.php');
+
+if (!isset($_SESSION['username']))
+{
+	header('Location: index.php');
+}
+
+$template = new FITemplate('main');
+$template->add('SITENAME',instaDisc_getConfig('siteName'));
+$template->display();
+
+?>
