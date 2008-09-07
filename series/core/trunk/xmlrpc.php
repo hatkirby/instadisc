866a1d8567457b451d61339064b52973419b2b0b
Kelly Rauchenberger <fefferburbia@gmail.com>
2008-09-07 13:33:30 +0000

    Series: Added getPasswordInfo() to XML-RPC interface
    Refs #28

--- series/core/trunk/xmlrpc.php
+++ series/core/trunk/xmlrpc.php
@@ -17,7 +17,26 @@ function subscriptionInfo($id)
 			));
 }
 
-$s = new xmlrpc_server(array(	"InstaDisc.subscriptionInfo" => array('function' => 'subscriptionInfo')
+function getPasswordInfo($id)
+{
+	if (!instaDisc_subscriptionExists($id))
+	{
+		return new xmlrpcresp(new xmlrpcval('false', 'string'));
+	}
+
+	$sub = instaDisc_getSubscription($id);
+	if ($sub['password'] == '')
+	{
+		return new xmlrpcresp(new xmlrpcval('false', 'string'));
+	} else {
+		$verID = rand(1,2147483647);
+
+		return new xmlrpcresp(new xmlrpcval(md5($sub['password'] . ':' . $verID) . ':' . $verID, 'string'));
+	}
+}
+
+$s = new xmlrpc_server(array(	"InstaDisc.subscriptionInfo" => array('function' => 'subscriptionInfo'),
+				"InstaDisc.getPasswordInfo" => array('function' => 'getPasswordInfo')
 			), 0);
 $s->functions_parameters_type = 'phpvals';
 $s->service();
