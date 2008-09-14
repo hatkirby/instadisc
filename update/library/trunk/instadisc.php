9537a06af9d379e759b5e1defe844d5545550112
hatkirby <hatkirby@localhost>
2008-09-14 14:07:45 +0000

    Update: Reverted Series integration
    Refs #55

--- update/library/trunk/instadisc.php
+++ update/library/trunk/instadisc.php
@@ -6,18 +6,12 @@ include('xmlrpc/xmlrpc.inc');
 
 $idusSubscriptionSeriesURL = array();
 $idusSubscriptionID = array();
-$idusSeriesUsername = array();
-$idusSeriesPassword = array();
-$idusSubscriptionURL = array();
-$idusSubscriptionTitle = array();
-$idusSubscriptionCategory = array();
-$idusSubscriptionPersonal = array();
 $idusEncryptionKey = array();
 $instaDisc_subCount = 0;
 
 function instaDisc_sendItem($id, $title, $author, $url, $semantics)
 {
-	global $idusSubscriptionSeriesURL, $idusSubscriptionID, $idusSeriesUsername, $idusSeriesPassword, $idusSubscriptionURL, $idusSubscriptionTitle, $idusSubscriptionCategory, $idusSubscriptionPersonal, $idusEncryptionKey;
+	global $idusSubscriptionSeriesURL, $idusSubscriptionID, $idusEncryptionKey;
 
 	$encID = 0;
 	if (($idusEncryptionKey[$id] != '') && extension_loaded('mcrypt'))
@@ -44,16 +38,9 @@ function instaDisc_sendItem($id, $title, $author, $url, $semantics)
 	
 	$verID = rand(1,2147483647);
 
-	$client = new xmlrpc_client($idusSubscriptionSeriesURL[$id]);
-	$msg = new xmlrpcmsg("InstaDisc.sendFromUpdate", array(	new xmlrpcval($idusSeriesUsername[$id], 'string'),
-								new xmlrpcval(md5($idusSeriesUsername[$id] . ':' . md5($idusSeriesPassword[$id]) . ':' . $verID), 'string'),
-								new xmlrpcval($verID, 'int'),
-								new xmlrpcval($idusSubscriptionSeriesURL[$id], 'string'),
+	$client = new xmlrpc_client('http://central.fourisland.com/xmlrpc.php');
+	$msg = new xmlrpcmsg("InstaDisc.sendFromUpdate", array(	new xmlrpcval($idusSubscriptionSeriesURL[$id], 'string'),
 								new xmlrpcval($idusSubscriptionID[$id], 'string'),
-								new xmlrpcval($idusSubscriptionURL[$id], 'string'),
-								new xmlrpcval($idusSubscriptionTitle[$id], 'string'),
-								new xmlrpcval($idusSubscriptionCategory[$id], 'string'),
-								new xmlrpcval($idusSubscriptionPersonal[$id], 'string'),
 								new xmlrpcval($title, 'string'),
 								new xmlrpcval($author, 'string'),
 								new xmlrpcval($url, 'string'),
@@ -73,17 +60,11 @@ function instaDisc_sendItem($id, $title, $author, $url, $semantics)
 	}
 }
 
-function instaDisc_addSubscription($url, $id, $un, $pw, $sUrl, $title, $cat, $personal = '', $enc = '')
+function instaDisc_addSubscription($url, $id, $enc = '')
 {
-	global $instaDisc_subCount, $idusSubscriptionSeriesURL, $idusSubscriptionID, $idusSeriesUsername, $idusSeriesPassword, $idusSubscriptionURL, $idusSubscriptionTitle, $idusSubscriptionCategory, $idusSubscriptionPersonal, $idusEncryptionKey;
+	global $instaDisc_subCount, $idusSubscriptionSeriesURL, $idusSubscriptionID, $idusEncryptionKey;
 	$idusSubscriptionSeriesURL[$instaDisc_subCount] = $url;
 	$idusSubscriptionID[$instaDisc_subCount] = $id;
-	$idusSeriesUsername[$instaDisc_subCount] = $un;
-	$idusSeriesPassword[$instaDisc_subCount] = $pw;
-	$idusSubscriptionURL[$instaDisc_subCount] = $sUrl;
-	$idusSubscriptionTitle[$instaDisc_subCount] = $title;
-	$idusSubscriptionCategory[$instaDisc_subCount] = $cat;
-	$idusSubscriptionPersonal[$instaDisc_subCount] = ($personal != '' ? 'true' : 'false');
 	$idusEncryptionKey[$instaDisc_subCount] = $enc;
 	$instaDisc_subCount++;
 }
