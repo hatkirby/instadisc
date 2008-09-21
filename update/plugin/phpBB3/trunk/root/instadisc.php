f3fb5cc8f12a6916ef9325da7fb08f40dd64767c
hatkirby <hatkirby@localhost>
2008-09-21 23:44:04 +0000

    Update: Added SF generator to phpBB3
    Fixes #60

---
+++ update/plugin/phpBB3/trunk/root/instadisc.php
@@ -0,0 +1,55 @@
+<?php
+/**
+*
+* @package phpBB3
+* @version $Id: instadisc.php 2008-08-06 07:12:00Z hatkirby $
+* @version (c) 2008 Starla Insigna
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+/**
+* @ignore
+*/
+define('IN_PHPBB', true);
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+include($phpbb_root_path . 'common.' . $phpEx);
+include($phpbb_root_path . 'includes/functions_display.' . $phpEx);
+
+// Start session
+$user->session_begin();
+$auth->acl($user->data);
+
+$user->setup('mods/instadisc', $forum_data['forum_style']);
+
+$template->set_filenames(array(
+	'body' => 'instadisc.html')
+);
+
+$template->assign_vars(array(
+	'S_SUBSCRIPTION'	=> ('http://' . $_SERVER['SERVER_NAME'] . '/forum-post/' . generateSlug($config['id_subscription_title']) . '/'),
+	'S_TITLE'		=> $config['id_subscription_title'],
+	'S_KEY'			=> $config['id_activation_key'],
+));
+
+page_footer();
+
+function generateSlug($title)
+{
+        $title = preg_replace('/[^A-Za-z0-9]/','-',$title);
+        $title = preg_replace('/-{2,}/','-',$title);
+        if (substr($title,0,1) == '-')
+        {
+                $title = substr($title,1);
+        }
+        if (substr($title,strlen($title)-1,1) == '-')
+        {
+                $title = substr($title,0,strlen($title)-1);
+        }
+        $title = strtolower($title);
+
+        return($title);
+}
+
+?>
