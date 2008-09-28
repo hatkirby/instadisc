69c1ceb95567798eefc5be899489df25def41767
hatkirby <hatkirby@localhost>
2008-09-28 14:33:18 +0000

    Client: Completed step 6
    Refs #69

--- client/trunk/src/com/fourisland/instadisc/DownloadItem/DownloadItemModeTest.java
+++ client/trunk/src/com/fourisland/instadisc/DownloadItem/DownloadItemModeTest.java
@@ -106,6 +106,10 @@ public class DownloadItemModeTest extends javax.swing.JDialog {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        XmlRpc xmlrpc = new XmlRpc("setDownloadItemMode");
+        xmlrpc.addParam(Wrapper.getConfig("downloadItemMode"));
+        xmlrpc.execute();
+        
         StepEndResults.ok = true;
         this.setVisible(false);
     }//GEN-LAST:event_jButton1ActionPerformed
