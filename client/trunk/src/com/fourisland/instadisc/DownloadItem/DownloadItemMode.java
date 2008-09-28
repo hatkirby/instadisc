509b083d15431af33dba6d07a280d3bdd5f09b89
hatkirby <hatkirby@localhost>
2008-09-28 15:59:06 +0000

    Client: Worked more on step 2
    Refs #69

--- client/trunk/src/com/fourisland/instadisc/DownloadItem/DownloadItemMode.java
+++ client/trunk/src/com/fourisland/instadisc/DownloadItem/DownloadItemMode.java
@@ -1,13 +1,12 @@
 package com.fourisland.instadisc.DownloadItem;
 
-import java.util.Timer;
-
 public interface DownloadItemMode
 {
     public void modeInitalize();
     public void modeDeinitalize();
     
     public void requestRetained();
+    public void sendItem(int id);
     
     public int setTimer();
     public void timerTick();
