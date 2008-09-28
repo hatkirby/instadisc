6f881a9f11d80e2bc27ec5ade72ac55eaef495c6
Kelly Rauchenberger <fefferburbia@gmail.com>
2008-09-28 22:53:48 +0000

    Central: Added createUser()
    Refs #63

--- central/trunk/instadisc.sql
+++ central/trunk/instadisc.sql
@@ -84,7 +84,6 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL auto_increment,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
   `ip` varchar(255) NOT NULL,
   `port` int(11) NOT NULL,
   `nextItemID` int(11) NOT NULL,
--- central/trunk/install.php
+++ central/trunk/install.php
@@ -2,10 +2,6 @@
 
 /* InstaDisc Server - A Four Island Project */
 
-include('includes/class.phpmailer.php');
-
-$softwareVersion = 1;
-
 if (!isset($_GET['submit']))
 {
 	showHeader('1');
@@ -90,132 +86,49 @@ if (!isset($_GET['submit']))
 			break;
 
 		case 2:
-			if ($_POST['smtpHost'] == '')
+			if ($_POST['adminUser'] == '')
 			{
-				addError($numOfErrors, $errors, 'smtpHost', 'SMTP Host is a required field');
+				addError($numOfErrors, $errors, 'adminUser', 'Admin Username is a required field');
 			}
 
-			if ($_POST['smtpAuth'] == 'on')
+			if ($_POST['adminPass'] == '')
 			{
-				if ($_POST['smtpUser'] == '')
-				{
-					addError($numOfErrors, $errors, 'smtpUser', 'When "SMTP Authentication Required?" is checked, SMTP Username is a required field');
-				}
-
-				if ($_POST['smtpPass'] == '')
-				{
-					addError($numOfErrors, $errors, 'smtpPass', 'When "SMTP Authentication Required?" is checked, SMTP Password is a required field');
-				}
+				addError($numOfErrors, $errors, 'adminPass', 'Admin Password is a required field');
 			}
 
 			if ($numOfErrors > 0)
 			{
 				showHeader('2');
-				showStepTwo($_POST['mailDomain'], $_POST['smtpHost'], ($_POST['smtpAuth'] == 'on' ? ' CHECKED' : ''), $_POST['smtpUser'], $_POST['smtpPass'], $_POST['siteName'], $_POST['xmlrpcURL'], $_POST['adminUser'], $_POST['adminPass'], $_POST['adminEmail'], $errors);
+				showStepTwo($_POST['adminUser'], $_POST['adminPass'], $errors);
 			} else {
-			        $mail = new PHPMailer();
-			        $mail->IsSMTP();
-			        $mail->From = 'instadisc@' . $_POST['mailDomain'];
-			        $mail->FromName = 'InstaDisc';
-			        $mail->Host = $_POST['smtpHost'];
-			        if ($_POST['smtpAuth'] == 'on')
-			        {
-			                $mail->SMTPAuth = true;
-			                $mail->Username = $_POST['smtpUser'];
-			                $mail->Password = $_POST['smtpPass'];
-			        }
-			        $mail->Helo = $_SERVER['HTTP_HOST'];
-			        $mail->ClearAddresses();
-				$mail->AddAddress("test@fourisland.com");
-				$mail->Subject = 'Test Email';
-				$mail->Body = 'Please discard this email.';
-				$mail->Send();
-				if ($mail->IsError())
-				{
-					addError($numOfErrors, $errors, '', $mail->ErrorInfo);
-				}
+				include_once('includes/config.php');
 
-				if ($_POST['mailDomain'] == '')
-				{
-					addError($numOfErrors, $errors, 'mailDomain', 'Mail Domain is a required field');
-				}
+				mysql_connect($dbhost, $dbuser, $dbpass);
+				mysql_select_db($dbname);
 
-				if ($_POST['siteName'] == '')
-				{
-					addError($numOfErrors, $errors, 'siteName', 'Site Name is a required field');
-				}
+				$sql[0] = "INSERT INTO config (name,value) VALUES (\"owner\",\"" . mysql_real_escape_string($_POST['adminUser']) . "\")";
+				$sql[1] = "INSERT INTO config (name,value) VALUES (\"verIDBufferSize\",\"10000\")";
+				$sql[2] = "INSERT INTO users (username, password, ip) VALUES (\"" . mysql_real_escape_string($_POST['adminUser']) . "\",\"" . mysql_real_escape_string(md5($_POST['adminPass'])) . "\",\"" . mysql_real_escape_string($_SERVER['REMOTE_ADDR']) . "\")";
 
-				if ($_POST['xmlrpcURL'] == '')
+				foreach ($sql as $name => $value)
 				{
-					addError($numOfErrors, $errors, 'xmlrpcURL', 'XML-RPC URL is a required field');
-				} else {
-					include_once('includes/xmlrpc/xmlrpc.inc');
-
-					$client = new xmlrpc_client($_POST['xmlrpcURL']);
-					$msg = new xmlrpcmsg('system.listMethods');
-					$r = $client->send($msg);
-					if (stripos($r->faultString(),'Connect error') !== FALSE)
+					if (!trim($value) == '')
 					{
-						addError($numOfErrors, $errors, 'xmlrpcURL', $r->faultString());
+						$sql2 = @mysql_query($value);
+						if (!$sql2)
+						{
+							addError($numOfErrors, $errors, '', "MySQL error \"" . mysql_error() . "\" while filling database");
+						}
 					}
-				}
-
-				if ($_POST['adminUser'] == '')
-				{
-					addError($numOfErrors, $errors, 'adminUser', 'Admin Username is a required field');
-				}
-
-				if ($_POST['adminPass'] == '')
-				{
-					addError($numOfErrors, $errors, 'adminPass', 'Admin Password is a required field');
-				}
-
-				if ($_POST['adminEmail'] == '')
-				{
-					addError($numOfErrors, $errors, 'adminEmail', 'Admin Email is a required field');
 				}
 
 				if ($numOfErrors > 0)
 				{
 					showHeader('2');
-					showStepTwo($_POST['mailDomain'], $_POST['smtpHost'], ($_POST['smtpAuth'] == 'on' ? ' CHECKED' : ''), $_POST['smtpUser'], $_POST['smtpPass'], $_POST['siteName'], $_POST['xmlrpcURL'], $_POST['adminUser'], $_POST['adminPass'], $_POST['adminEmail'], $errors);
+					showStepTwo($_POST['adminUser'], $_POST['adminPass'], $errors);
 				} else {
-					include_once('includes/config.php');
-
-					mysql_connect($dbhost, $dbuser, $dbpass);
-					mysql_select_db($dbname);
-
-					$sql[0] = "INSERT INTO config (name,value) VALUES (\"mailDomain\",\"" . mysql_real_escape_string($_POST['mailDomain']) . "\")";
-					$sql[1] = "INSERT INTO config (name,value) VALUES (\"smtpHost\",\"" . mysql_real_escape_string($_POST['smtpHost']) . "\")";
-					$sql[2] = "INSERT INTO config (name,value) VALUES (\"smtpAuth\",\"" . mysql_real_escape_string(($_POST['smtpAuth'] == 'on' ? 'true' : 'false')) . "\")";
-					$sql[3] = "INSERT INTO config (name,value) VALUES (\"smtpUser\",\"" . mysql_real_escape_string($_POST['smtpUser']) . "\")";
-					$sql[4] = "INSERT INTO config (name,value) VALUES (\"smtpPass\",\"" . mysql_real_escape_string($_POST['smtpPass']) . "\")";
-					$sql[5] = "INSERT INTO config (name,value) VALUES (\"siteName\",\"" . mysql_real_escape_string($_POST['siteName']) . "\")";
-					$sql[6] = "INSERT INTO config (name,value) VALUES (\"xmlrpcURL\",\"" . mysql_real_escape_string($_POST['xmlrpcURL']) . "\")";
-					$sql[7] = "INSERT INTO config (name,value) VALUES (\"owner\",\"" . mysql_real_escape_string($_POST['adminUser']) . "\")";
-					$sql[8] = "INSERT INTO config (name,value) VALUES (\"verIDBufferSize\",\"10000\")";
-					$sql[11] = "INSERT INTO users (username, password, email, ip) VALUES (\"" . mysql_real_escape_string($_POST['adminUser']) . "\",\"" . mysql_real_escape_string(md5($_POST['adminPass'])) . "\",\"" . mysql_real_escape_string($_POST['adminEmail']) . "\",\"" . mysql_real_escape_string($_SERVER['REMOTE_ADDR']) . "\")";
-
-					foreach ($sql as $name => $value)
-					{
-						if (!trim($value) == '')
-						{
-							$sql2 = @mysql_query($value);
-							if (!$sql2)
-							{
-								addError($numOfErrors, $errors, '', "MySQL error \"" . mysql_error() . "\" while filling database");
-							}
-						}
-					}
-
-					if ($numOfErrors > 0)
-					{
-						showHeader('2');
-						showStepTwo($_POST['mailDomain'], $_POST['smtpHost'], ($_POST['smtpAuth'] == 'on' ? ' CHECKED' : ''), $_POST['smtpUser'], $_POST['smtpPass'], $_POST['siteName'], $_POST['xmlrpcURL'], $_POST['adminUser'], $_POST['adminPass'], $_POST['adminEmail'], $errors);
-					} else {
-						showHeader('3');
-						showStepThree();
-					}
+					showHeader('3');
+					showStepThree();
 				}
 			}
 
@@ -270,7 +183,7 @@ function showStepOne($host, $username, $password, $dbname, $errors)
 </DIV></FORM><?php
 }
 
-function showStepTwo($mailDomain, $smtpHost, $smtpAuth, $smtpUser, $smtpPass, $siteName, $xmlrpcURL, $adminUser, $adminPass, $adminEmail, $errors)
+function showStepTwo($adminUser, $adminPass, $errors)
 {
 ?>Your database has been set up. All we need to do now is fill it up a little. Please answer the below questions to set up your configuration:
 <FORM CLASS="uniform" ACTION="./install.php?submit=2" METHOD="POST">
@@ -286,43 +199,7 @@ function showStepTwo($mailDomain, $smtpHost, $smtpAuth, $smtpUser, $smtpPass, $s
 ?></OL></DIV><?php
 	}
 ?>
-<FIELDSET CLASS="inlineLabels"><LEGEND>Email</LEGEND>
-<DIV CLASS="ctrlHolder<?php ifErrors($errors, 'mailDomain'); ?>">
-<?php doErrors($errors, 'mailDomain'); ?> <LABEL FOR="mailDomain"><EM>*</EM> Mail Domain: </LABEL>
- <INPUT TYPE="text" ID="mailDomain" NAME="mailDomain" CLASS="textInput" VALUE="<?php echo($mailDomain); ?>">
- <P CLASS="formHint">Type in the part that comes after the @ in your email addresses. This is used when InstaDisc needs to send an email to someone.</P>
-</DIV>
-<DIV CLASS="ctrlHolder<?php ifErrors($errors, 'smtpHost'); ?>">
-<?php doErrors($errors, 'smtpHost'); ?> <LABEL FOR="smtpHost"><EM>*</EM> SMTP Host: </LABEL>
- <INPUT TYPE="text" ID="smtpHost" NAME="smtpHost" CLASS="textInput" VALUE="<?php echo($smtpHost); ?>">
- <P CLASS="formHint">This is required because InstaDisc has to be able to send emails to people.</P>
-</DIV>
-<DIV CLASS="ctrlHolder<?php ifErrors($errors, 'smtpAuth'); ?>">
-<?php doErrors($errors, 'smtpAuth'); ?> <LABEL FOR="smtpAuth">SMTP Authentication Required? </LABEL>
- <INPUT TYPE="checkbox" ID="smtpAuth" NAME="smtpAuth" CLASS="textInput"<?php echo($smtpAuth); ?>">
- <P CLASS="formHint">If your SMTP server requires authentication (most do), you need to check this box and enter the authentication details in the fields below.</P>
-</DIV>
-<DIV CLASS="ctrlHolder<?php ifErrors($errors, 'smtpUser'); ?>">
-<?php doErrors($errors, 'smtpUser'); ?> <LABEL FOR="smtpUser">SMTP Username: </LABEL>
- <INPUT TYPE="text" ID="smtpUser" NAME="smtpUser" CLASS="textInput" VALUE="<?php echo($smtpUser); ?>">
-</DIV>
-<DIV CLASS="ctrlHolder<?php ifErrors($errors, 'smtpPass'); ?>">
-<?php doErrors($errors, 'smtpPass'); ?> <LABEL FOR="smtpPass">SMTP Password: </LABEL>
- <INPUT TYPE="password" ID="smtpPass" NAME="smtpPass" CLASS="textInput" VALUE="<?php echo($smtpPass); ?>">
- <P CLASS="formHint">The two above fields only need be filled out if the "SMTP Authentication Required?" box is checked.</P>
-</DIV>
-</FIELDSET><FIELDSET CLASS="inlineLabels"><LEGEND>Website</LEGEND>
-<DIV CLASS="ctrlHolder<?php ifErrors($errors, 'siteName'); ?>">
-<?php doErrors($errors, 'siteName'); ?> <LABEL FOR="siteName"><EM>*</EM> Site Name: </LABEL>
- <INPUT TYPE="text" ID="siteName" NAME="siteName" CLASS="textInput" VALUE="<?php echo($siteName); ?>">
- <P CLASS="formHint">Your website's name is required for a little personalization of emails.</P>
-</DIV>
-<DIV CLASS="ctrlHolder<?php ifErrors($errors, 'xmlrpcURL'); ?>">
-<?php doErrors($errors, 'xmlrpcURL'); ?> <LABEL FOR="xmlrpcURL"><EM>*</EM> XML-RPC URL: </LABEL>
- <INPUT TYPE="text" ID="xmlrpcURL" NAME="xmlrpcURL" CLASS="textInput" VALUE="<?php echo($xmlrpcURL); ?>">
- <P CLASS="formHint">What is the URL of the xmlrpc.php file provided for you in the InstaDisc package?</P>
-</DIV>
-</FIELDSET><FIELDSET CLASS="inlineLabels"><LEGEND>Administrator's Account</LEGEND>
+<FIELDSET CLASS="inlineLabels"><LEGEND>Administrator's Account</LEGEND>
 <DIV CLASS="ctrlHolder<?php ifErrors($errors, 'adminUser'); ?>">
 <?php doErrors($errors, 'adminUser'); ?> <LABEL FOR="adminUser"><EM>*</EM> Admin Username: </LABEL>
  <INPUT TYPE="text" ID="adminUser" NAME="adminUser" CLASS="textInput" VALUE="<?php echo($adminUser); ?>">
@@ -330,11 +207,6 @@ function showStepTwo($mailDomain, $smtpHost, $smtpAuth, $smtpUser, $smtpPass, $s
 <DIV CLASS="ctrlHolder<?php ifErrors($errors, 'adminPass'); ?>">
 <?php doErrors($errors, 'adminPass'); ?> <LABEL FOR="adminPass"><EM>*</EM> Admin Password: </LABEL>
  <INPUT TYPE="password" ID="adminPass" NAME="adminPass" CLASS="textInput" VALUE="<?php echo($adminPass); ?>">
-</DIV>
-<DIV CLASS="ctrlHolder<?php ifErrors($errors, 'adminEmail'); ?>">
-<?php doErrors($errors, 'adminEmail'); ?> <LABEL FOR="adminEmail"><EM>*</EM> Admin Email: </LABEL>
- <INPUT TYPE="text" ID="adminEmail" NAME="adminEmail" CLASS="textInput" VALUE="<?php echo($adminEmail); ?>">
- <P CLASS="formHint">You, the administrator, must have an account on your InstaDisc server to be able to edit configuration values (mostly the ones you just entered) at will.</P>
 </DIV>
 </FIELDSET>
 <DIV CLASS="buttonHolder">
@@ -344,7 +216,7 @@ function showStepTwo($mailDomain, $smtpHost, $smtpAuth, $smtpUser, $smtpPass, $s
 
 function showStepThree()
 {
-?>Congradulations! You've successfully set up your InstaDisc Central Server's database! Now, the next step for you is to implement the functions in instadisc.php into your web application. See <A HREF="http://fourisland.com/projects/instadisc/wiki/BecomingACentralServer">Becoming A Central Server</A>. Also, it would be smart to subscribe to your InstaDisc Update Notice Subscription, which will notify you if your Central Server's software gets out of date. It's the subscription.php file in this directory. Please subscribe to it, thanks!<P>However, remember that to be any use to anyone, you need your Central Server database, and to be added to others'. To do this, please find the appropriate help section on http://fourisland.com/projects/instadisc/BecomingACentralServer<?php
+?>Congradulations! You've successfully set up your InstaDisc Central Server's database!<?php
 }
 
 function ifErrors($errors, $id)
